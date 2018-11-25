aba1e4d1a16ce5a9611d17e971d664995f8c40b0
kopec <pawelkopec10@gmail.com>
2018-11-25 04:29:22 +0100

    rest

--- rest/app.py
+++ rest/app.py
@@ -1,13 +1,34 @@
 from flask import Flask, jsonify
+import json
+
+
+def load_regions():
+    with open('data/regions.json') as f:
+        return json.load(f)
+
+
+def provide_metrics(location):
+    return [
+        {"metric1": 3},
+        {"metric2": 4},
+        {"metric3": 5}
+    ]
+
 
 app = Flask(__name__)
+app.config.regions = load_regions()
 
 
-@app.route('/', methods=['GET'])
-def health_scores():
-    scores = {str(w): {'health': w} for w in range(16)}
-    return jsonify(scores)
+@app.route('/regions', methods=['GET'])
+def regions():
+    return jsonify(app.config.regions)
+
+
+@app.route('/metrics', methods=['POST'])
+def metrics():
+    location = request.json
+    return jsonify(provide_metrics(location))
+
 
 if __name__ == '__main__':
     app.run()
-
